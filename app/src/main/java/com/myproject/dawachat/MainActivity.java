b84c05e0957f06d5141cee79c57dc3a9f7a9919e
yassinhasan <figo781@gmail.com>
2023-01-03 18:32:19 +0300

    add sign up page

--- app/src/main/java/com/myproject/dawachat/MainActivity.java
+++ app/src/main/java/com/myproject/dawachat/MainActivity.java
@@ -2,7 +2,11 @@ package com.myproject.dawachat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
---
+++ app/src/main/java/com/myproject/dawachat/SignUpActitivty.java
@@ -0,0 +1,17 @@
+package com.myproject.dawachat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myproject.dawachat.databinding.ActivitySignUpActitivtyBinding;
+
+public class SignUpActitivty extends AppCompatActivity {
+
+    ActivitySignUpActitivtyBinding activitySignUpActitivtyBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_actitivty);
+    }
+}
